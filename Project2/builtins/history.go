1eaa4ff2313607a1fd3be690dce598b80bd01c54
OliviaCantrell <123116908+OliviaCantrell@users.noreply.github.com>
2023-05-12 14:11:09 -0500

    Create history.go

---
+++ Project2/builtins/history.go
@@ -0,0 +1,11 @@
+var history []string
+
+func addHistory(cmd string) {
+    history = append(history, cmd)
+}
+
+func listHistory() {
+    for i, cmd := range history {
+        fmt.Printf("%d: %s\n", i+1, cmd)
+    }
+}
